7df17b3d51d4c8db74b9e2bb12f5dd72e917ae17
Katherina <vovk.katya@gmail.com>
2019-09-09 16:25:29 +0300

    edit request form

--- backend/views/requests/create.php
+++ backend/views/requests/create.php
@@ -11,10 +11,21 @@ $this->title = 'Создать заявку';
 //$this->params['breadcrumbs'][] = ['label' => 'Requests', 'url' => ['index']];
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="requests-create">
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+<div class="col col-md-9">
+        <div class="box">
+             <div class="box-header with-border">
+                        <h4><i class="glyphicon glyphicon-envelope"></i> Создать заявку</h4>
+                        <span>Общие требования:</span>
+                        <div class="pull-right box-tools">
+                            <?php // Html::a('<i class="fa fa-times"></i>', ['#'], ['class' => 'btn btn-info btn-xs']) ?>
+                        </div>
+            </div>
+            <div class="box-body">
+                <div class="requests-create">
+                    <?= $this->render('_form', [
+                        'model' => $model,
+                    ]) ?>
+                </div>
+            </div>
+        </div>
 </div>
