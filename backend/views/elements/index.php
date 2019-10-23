66f0db9479e3fb6938c7f8ec5ff42a28bab06e79
yuliyagord <oxalis.torrents.2@gmail.com>
2019-10-23 16:29:06 +0300

    elements/index: decrease in the number of queries, search, sort and "Will be"

--- backend/views/elements/index.php
+++ backend/views/elements/index.php
@@ -10,231 +10,127 @@ use yii\bootstrap\Modal;
 
 use common\models\Category;
 use common\models\Elements;
-/* @var $this yii\web\View */
-/* @var $searchModel backend\models\elementsSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+use common\models\Produce;
 
 $this->title = yii::t('app', 'Elements');
 $this->params['breadcrumbs'][] = $this->title;
-
-$gridColumns = [
-   ['class' => 'kartik\grid\SerialColumn'],
-    'idelements', 
-    'box',
-    [
-      'attribute' => 'name',
-      'label' => 'ValueManufacturerPartNumber', 
-    ],
-    [
-      'attribute' => 'nominal',
-      'label' => 'ValueDescription', 
-    ],
-    'quantity',
-    [
-        'attribute' => 'idproduce',
-        'label'  => 'ValueManufacturer ',
-        'value' => 'produce.manufacture',
-    ],
-    [
-        'attribute' => 'active',
-        'value' => function($data){
-                    if($data->active == 1){
-                        return 'Актуально';
-                    }elseif($data->active == 2){
-                        return 'Устарело';
-                    }
-                   
-                },
-    ],
-    
-];
-
 ?>
 <div class="elements-index">
-    <p>
-        <?php echo Html::a(yii::t('app', 'Create new element'), ['create'], ['class' => 'btn btn-success']) ?>
-      
-       
-    </p>
+    <p><?php echo Html::a(yii::t('app', 'Create new element'), ['create'], ['class' => 'btn btn-success']) ?></p>
     <div class="search-form">
         <div class="box box-solid bg-gray-light" style="background: #fff;">
             <div class="box-body">
-                <span><?= yii::t('app', 'Search by')?>:</span>
-                <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+                <?php  
+                    echo $this->render('_search', [
+                        'model' => $searchModel, 
+                        'aCategoryHierarchy' => $aCategoryHierarchy,
+                        'aProduce' => $aProduce,
+                    ]);
+                ?>
             </div>
         </div>
     </div>
    
-<?php Pjax::begin(['id' => 'elements']); ?>  
+<?php 
+    //Pjax::begin(['id' => 'elements']); 
+?>  
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'pjax' => true,
-       // 'bordered' => true,
-        'striped' => true,
-        'condensed' => true,
-        'responsive' => true,
-        'hover' => true,
-        'columns' => $gridColumns,
-        'resizableColumns'=>true,
-      //  'fontAwesome' => true,
-
-        // 'floatHeader' => true,
-        'tableOptions' => [
-            'class' => 'table table-striped table-bordered'
-        ],
-        'containerOptions' => ['style'=>'overflow: auto'], // only set when $responsive = false
+//        'pjax' => true,
+//        'striped' => true, //полосатый стиль
+//        'condensed' => true,
+//        'responsive' => true,
+//        'hover' => true,
+//        'resizableColumns' => true,
+//        'tableOptions' => [
+//            'class' => 'table table-striped table-bordered'
+//        ],
+        'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
         'columns' => [
-        /*    [
-                'class' => 'yii\grid\ActionColumn',
-               // 'contentOptions' => ['style' => 'width:45px;'],
-                  'buttons' => [
-                'prices' => function ($url,$model,$key) {
-                  $url = Url::to(['viewprice', 'idel' => $key]);
-                    return Html::a('<span class="glyphicon glyphicon-euro"></span>', $url,
-                            ['title' => 'Посмотреть цену']
-                            );
-                },
-            ],*/
-         //   ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute' => 'image',
-                'label' => 'Image',
-                'format' => 'raw',
-                'contentOptions' =>['class' => 'table_class','style'=>'display:block;'],
-                'value' => function ($model) {
-                  //  if($data->image){
-                         return Html::img(Yii::getAlias('@web').'/images/' . $model->image,//$data['image'],
-                               
-                         ['width' => '60px']);
-                //    }else{
-                //        return '<span>no image</span>';
-                 //   }
-                  // return $model->displayImage;
-                },
-            ],
             [
                 'attribute' => 'idelements', 
-                'contentOptions'=>['style'=>'width: 40px;'],
+                'contentOptions' => ['style' => 'width: 40px;'],
             ],
             [
-                'attribute' =>  'box',
-                'contentOptions'=>['style'=>'width: 60px;'],
+                'attribute' => 'box',
+                'contentOptions' => ['style' => 'width: 60px;'],
             ],
             [
-               'attribute'=>'name',
-               'format'=>'raw',
-               'value' => function ($model, $key, $index) { 
+               'attribute' => 'name',
+               'format' => 'raw',
+               'value' => function($model, $key, $index) {
                     return Html::a($model->name, ['view', 'id' => $model->idelements]);
                 },
             ],
             [
                 'attribute' => 'nominal',
-                 'contentOptions'=>['style'=>'word-wrap: break-word'],
+                'contentOptions' => ['style' => 'word-wrap: break-word'],
             ],
-            
             [
                 'attribute' => 'quantity',
                 'label' => 'Onstock',
                 'format' => 'raw',
-                'value' => function($data, $index){
-                    
-                    if ($data->quantity == '0'){
-                        return '<center><strong>' . $data->quantity.'</strong></center>'. '<br/><center><small>Will be: '. $index . '</small></center>';
-                    }else{
-                        return '<center><strong>' . $data->quantity.'</strong></center>';
-                    }  
+                'value' => function($model, $index) {
+                    $sQty = '<center><strong>' . $model->quantity . '</strong></center>';
+                    if (!is_null($model->date_receive)) {
+                        return $sQty . '<center><small>Will be: ' . $model->expacted_quantity . ' (' . $model->date_receive . ')' . '</small></center>';
+                    }
+                    return $sQty;
                 },
-                'contentOptions'=>['style'=>'width: 50px;'],
+                'contentOptions' => ['style'=>'width: 50px;'],
             ],
             [
-                'attribute' => 'idproduce',
-             /*   'value' => function ($model) {
-                  //  return empty($model->idproduce) ? '-' : $model->produce->manufacture;
-                     return $model->produce->manufacture;
-                },*/
-               // 'value' => 'produce.manufacture',
-                'value' => function($data){
-                    return  empty($data->idproduce) ? '-' : HTML::a($data->produce->manufacture, ['produce/viewitem', 'id' => $data->idproduce]);
+                'attribute' => 'manufacture',
+                'value' => function($model){
+                    return Html::a($model->manufacture, ['produce/viewitem', 'id' => $model->idproduce]);
                 },
                 'format' => 'raw',
-                'filter' => Html::activeDropDownList($searchModel, 'idproduce', ArrayHelper::map(\common\models\Produce::find()->select(['idpr', 'manufacture'])->indexBy('idpr')->all(), 'idpr', 'manufacture'),
-                        ['class'=>'form-control','prompt' => yii::t('app', 'Choose manufacturer')]),
-                 'contentOptions'=>['style' => 'word-wrap: break-word'],
+                'filter' => Html::activeDropDownList($searchModel, 'idproduce', $aProduce, ['class'=>'form-control','prompt' => yii::t('app', 'Choose manufacturer')]),
+                'contentOptions' => ['style' => 'word-wrap: break-word; width: 14%;'],
             ],
             [
-                'attribute' => 'idcategory',
-                'value' => 'category.name',
-                'format' => 'raw',
-                'filter' => Html::activeDropDownList($searchModel, 'idcategory', Category::getHierarchy(), (['class'=>'form-control','prompt' => yii::t('app', 'Choose category')])),
-                'contentOptions' => ['style' => 'word-wrap: break-word'],
+                'attribute' => 'category_name',
+                'format' => 'text',
+                'filter' => Html::activeDropDownList($searchModel, 'idcategory', $aCategoryHierarchy, (['class'=>'form-control','prompt' => yii::t('app', 'Choose category')])),
+                'contentOptions' => ['style' => 'word-wrap: break-word; width: 14%'],
             ],
             [
                 'attribute' => 'active',
                 'format' => 'raw',
-                'value' => function($data){
-                    if($data->active == '1'){
+                'value' => function($data) {
+                    if ($data->active == '1') {
                         return '<span class="label label-success">Актуально</span>';
-                    }elseif($data->active == '2'){
+                    } elseif ($data->active == '2') {
                         return '<span class="label label-danger">Устарело</span>';
                     }
                    
                 },
-                'filter'=>['1' => 'Актуально', '2' => 'Снято с производства'],
+                'filter' => ['1' => 'Актуально', '2' => 'Устарело'],
                 'contentOptions' => ['style' => 'max-width: 90px;white-space: normal'],
             ],
             [
-            'class' => 'yii\grid\ActionColumn',
-            'contentOptions' => ['style' => 'width:45px;'],
-            'template' => '{update} {delete} {prices} {accounts} {viewfrom} {createfromquick}',
-            'buttons' => [
-                'prices' => function ($url,$model,$key) {
-                  $url = Url::to(['viewprice', 'idel' => $key]);
-                    return Html::a('<span class="glyphicon glyphicon-euro"></span>', $url,
-                            ['title' => 'Посмотреть цену']
-                            );
-                },
-                'accounts' => function ($url,$model,$key) {
-                  $url = Url::to(['tostock', 'idel' => $key]);
-                    return Html::a('<span class="glyphicon glyphicon-plus"></span>', $url,
-                            ['title' => 'Положить на склад']
-                            );
-                },
-                'viewfrom' => function ($url,$model,$key) {
-                  $url = Url::to(['viewfrom', 'idel' => $key]);
-                    return Html::a('<span class="glyphicon glyphicon-minus"></span>', $url,
-                            ['title' => 'Посмотреть что взято со склада']
-                            );
-                },
-                'createfromquick' => function ($url,$model,$key) {
-                  $url = Url::to(['createfromquick', 'idel' => $key]);
-                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url,
-                            ['title' => 'взять со склада быстро']
-                            );
-                },
-                        
-            'urlCreator' => function ($action, $model, $key, $index) {
-                 if ($action === 'receipt') {
-                     $url = yii::$app->controller->createUrl('receipt'); 
-                 return $url;}
-                 
-                if ($action === 'prices'){
-                     $url ='elements/viewprice?idel='.$model->idel;
-                return $url;}
-                
-                if ($action === 'fromstock'){
-                     $url ='fromstock/create?idelements='.$model->idelements;
-                     return $url;
-                }
-                
-                if ($action === 'createfromquick'){
-                     $url ='fromstock/create?idelements='.$model->idelements;
-                     return $url;
-                }
-                 },
+                'class' => 'yii\grid\ActionColumn',
+                'contentOptions' => ['style' => 'width:45px;'],
+                'template' => '{update} {accounts} {viewfrom} {createfromquick}',// {delete}
+                'buttons' => [
+                    'accounts' => function($url, $model, $key) {
+                        $url = Url::to(['tostock', 'idel' => $key]);
+                        return Html::a('<span class="glyphicon glyphicon-plus"></span>', $url, ['title' => 'Положить на склад']);
+                    },
+                    'viewfrom' => function($url, $model, $key) {
+                        $url = Url::to(['viewfrom', 'idel' => $key]);
+                        return Html::a('<span class="glyphicon glyphicon-minus"></span>', $url, ['title' => 'Посмотреть что взято со склада']);
+                    },
+                    'createfromquick' => function($url, $model, $key) {
+                        $url = Url::to(['createfromquick', 'idel' => $key]);
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, ['title' => 'Взять со склада быстро']);
+                    },
+                ],
             ],
         ],
-        ],
     ]); ?>
-<?php Pjax::end(); ?>  
+<?php 
+    // Pjax::end(); 
+?>  
 </div>
